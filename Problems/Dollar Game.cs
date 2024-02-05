e95661ff984e8913c689c43f4bb1eb536d9df785
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-02-05 15:46:25 -0800

    Comments added to dollar game,

--- Problems/Dollar Game.cs
+++ Problems/Dollar Game.cs
@@ -9,16 +9,22 @@
 
         private void checkButton_Click(object sender, EventArgs e)
         {
+            //Attempting to calculate the dollars with the provided inputs.
             try
             {
 
+                //Delcaring vairables to hold the amount of each coin entered and mutliplying them by their corresponding values.
                 decimal pennies = decimal.Parse(penniesTextBox.Text) * 0.01m; ;
                 decimal nickels = decimal.Parse(nickelsTextBox.Text) * 0.05m;
                 decimal dimes = decimal.Parse(dimsTextBox.Text) * 0.10m;
                 decimal quarters = decimal.Parse(quartersTextBox.Text) * 0.25m;
 
+                
+                //Adding the values of all the coins entered.
                 decimal total = (pennies + nickels + dimes + quarters);
-                if (total == 1.00m)
+
+                //Checking if the total is equal to, above, or below a dollar.
+                if (total == 1.00m) 
                 {
                     MessageBox.Show("Congrats! You made $1.00");
                 }else if (total > 1.00m)
@@ -30,7 +36,7 @@
                 }
 
 
-            }catch(Exception ex)
+            }catch(Exception ex) // Catching any invalid inputs.
             {
                 MessageBox.Show(ex.Message);
             }
